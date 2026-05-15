82e2af15e19434ef205060464433cc7839268984
Soumili Samanta <soumili.samanta18@gmail.com>
2026-05-15 06:17:56 +0530

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,22 @@
+/*smallest element*/
+
+import java.util.*;
+
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int smallest = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]<smallest){
+                smallest = arr[i];
+                
+            }
+        }
+        System.out.println(smallest);
+    }
+}
