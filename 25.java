1dd36f6190802a4bcb3e56801b60ec455ed8c72e
Soumili Samanta <soumili.samanta18@gmail.com>
2026-05-16 08:46:54 +0530

    Create 25.java

---
+++ 25.java
@@ -0,0 +1,25 @@
+// Remove duplicate from the sorted array
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        int j=0;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]!=arr[i+1]){
+                arr[j]=arr[i];
+                j++;
+            }
+        }
+        arr[j]=arr[n-1];
+        j++;
+        for(int i=0;i<j;i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
