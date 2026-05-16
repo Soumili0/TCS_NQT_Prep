6cf54cdb97c8953ab9a32645183cb74f56b64f3d
Soumili Samanta <soumili.samanta18@gmail.com>
2026-05-16 08:05:02 +0530

    Create 22.java

---
+++ 22.java
@@ -0,0 +1,21 @@
+// avg of elements in an array
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+
+    int total = 0;
+    for(int i=0;i<n;i++){
+      total+=arr[i];
+    }
+
+    int avg = total/n;
+    System.out.println(avg);
+    
+  }
+}
