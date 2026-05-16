2760f046b2a9ddd4b6abb26061fdc8a909f7d238
Soumili Samanta <soumili.samanta18@gmail.com>
2026-05-16 08:18:38 +0530

    Create 23.java

---
+++ 23.java
@@ -0,0 +1,20 @@
+//find median of the given array
+import java.util.*;
+public class Main{
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int arr[]=new int[n];
+    for(int i=0;i<n;i++){
+      arr[i]=sc.nextInt();
+    }
+Arrays.sort(arr);
+    int median = 0;
+    if(n%2==1){
+      median = arr[(n/2)];
+    }else{
+      median = (arr[n/2] + arr[(n/2)-1]) / 2;
+    }
+    System.out.println(median);
+  }
+}
